4f572386829d80244da15c23a9f6b8d60a05b6ff
unknown <girdharshukla01@gmail.com>
2022-05-18 06:30:10 +0530

    final

--- client/src/Chats.js
+++ client/src/Chats.js
@@ -1,13 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import TextEditor from './TextEditor';
+import React, { useCallback, useEffect, useState } from 'react'
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css'
+import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html'
 
 function Chats({socket,username,roomID}) {
 
-  const [message,setMessage] = useState('');
   const [messageList,setMessageList] = useState([]);
+  const [quill,setQuill] = useState()
+
+  const wrapperRef = useCallback((wrapper)=>{
+      if (wrapper==null) return
+      wrapper.innerHTML = ''
+      const editor = document.createElement('div')
+      wrapper.append(editor)
+      const q = new Quill(editor,{theme:'snow',
+      modules:{toolbar:
+      [
+          ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+          ['blockquote', 'code-block'],
+        
+          [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+          [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+          [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+          [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+          [{ 'direction': 'rtl' }],                         // text direction
+        
+          [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+          [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        
+          [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+          [{ 'font': [] }],
+          [{ 'align': [] }],
+        
+          ['clean']                                         // remove formatting button
+        ]}
+
+      })
+      setQuill(q)
+  },[])
 
   const sendMessage = async ()=>{
-    if(message!=='')
+    const message = quill.getContents()
+    if(message.ops[0].insert.trim()!=='')
     {
       const data = {
         roomID,message,username,time: new Date(Date.now()).getHours() +':'+ new Date(Date.now()).getMinutes()
@@ -29,12 +63,11 @@ function Chats({socket,username,roomID}) {
         <h3>Chats</h3>
         {messageList.map((message)=>(
           <>
-          <div className='message' id={message.username===username?'sender':'reciever'}>{message.message}</div>
+          <div className='message' id={message.username===username?'sender':'reciever'} dangerouslySetInnerHTML={{__html:new QuillDeltaToHtmlConverter(message.message.ops).convert()}} />
           <span className='user' id={message.username===username?'sender':'reciever'}>{message.username}  {message.time}</span>
           </>
         ))}
-        <TextEditor/>
-        {/* <input type='text' placeholder='Type your message...' onChange={(event)=>{setMessage(event.target.value)}} /> */}
+        <div id='container' ref={wrapperRef}></div>
         <button className='send' onClick={sendMessage}>Send</button>
     </>
   )
